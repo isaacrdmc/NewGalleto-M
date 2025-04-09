67c3030e36db78333b81e73822dccb35a1c0ee2c
JinnFlame <mendozaandres76517@gmail.com>
2025-04-09 01:59:44 -0600

    proveedores y rutas para cliente resuletos

--- modules/client/routes.py
+++ modules/client/routes.py
@@ -11,7 +11,7 @@ from .services import obtener_detalles_galletas
 @bp_clientes.route('/portal_cliente')
 @login_required
 def portal_cliente():
-    if current_user.rol.nombreRol not in ['Cliente', 'Administrador']:
+    if current_user.rol.nombreRol not in ['Cliente']:
         flash('No tienes permisos para acceder a esta sección', 'danger')
         return redirect(url_for('shared.index'))
     return render_template('client/portal_cliente.html')
@@ -19,8 +19,8 @@ def portal_cliente():
 @bp_clientes.route('/perfil')
 @login_required
 def perfil():
-    if 'username' not in session or session['role'] != 'cliente':
-        return redirect(url_for('admin.login'))
+    if current_user.rol.nombreRol not in ['Cliente']:
+        return redirect(url_for('shared.index'))
     return render_template('client/perfil_cliente.html')
 
 @bp_clientes.route('/pedidos', endpoint='pedidos_cliente')
@@ -28,7 +28,7 @@ def pedidos_cliente():
     """
     Renderiza la página de "Mis Pedidos" con los datos de la vista `vista_detalles_galletas`.
     """
-    if 'username' not in session or session['role'] != 'cliente':
-        return redirect(url_for('admin.login'))
+    if current_user.rol.nombreRol not in ['Cliente']:
+        return redirect(url_for('shared.index'))
     detalles_galletas = obtener_detalles_galletas()
     return render_template('client/pedidos_cliente.html', detalles_galletas=detalles_galletas)
