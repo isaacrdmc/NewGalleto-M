b3cce69cc12a95dcf0e0581d057800891e6f2a6a
Isaac <isaacrdmc@gmail.com>
2025-04-09 01:39:24 -0600

    Agregue los mensajes de erro correctamnte y trate de mmodificar la imágen a mostrar

--- __init__.py
+++ __init__.py
@@ -35,11 +35,11 @@ def create_app():
     # ^ Configuración de las rutas de error
     @app.errorhandler(404)
     def page_not_found(e):
-        return render_template('404.html'), 404
+        return render_template('shared/404.html'), 404
 
     @app.errorhandler(500)
     def internal_server_error(e):
-        return render_template('500.html'), 500
+        return render_template('shared/500.html'), 500
 
 
 
